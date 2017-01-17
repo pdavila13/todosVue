3fa3a771660a6eb730631324756c821a4d5b85c4
Paolo Davila <pdavila@iesebre.com>
2017-01-17 21:56:25 +0100

    Sync TODO

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,6 +1,12 @@
 <template>
     <div>
-        <div>{{ msg }}</div>
+        <div>
+            <ul>
+                <li v-for="(todo, index) in todos">
+                    {{ todo.name }}
+                </li>
+            </ul>
+        </div>
     </div>
 </template>
 <style>
@@ -13,7 +19,23 @@ export default {
   name: 'todos',
   data () {
     return {
-      msg: 'TODOS HERE!!'
+      todos: []
+    }
+  },
+  created () {
+    console.log('Component todolist created.')
+    this.fetchData()
+  },
+  methods: {
+    fetchData: function () {
+      return this.fetchPage(1)
+    },
+    fetchPage: function (page) {
+      this.$http.get('http://localhost:8002/api/v1/task?page=' + page).then((response) => {
+        console.log(response)
+      }, (response) => {
+        console.log(response)
+      })
     }
   }
 }
