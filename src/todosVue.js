af9a3cbbfe59f187d8809845d281e5a1bff03246
Paolo Dávila <pdavila@iesebre.com>
2017-04-20 19:30:39 +0200

    Modify todosVue.js

--- src/todosVue.js
+++ src/todosVue.js
@@ -1,7 +1,7 @@
 const todosVue = {
   STORAGE_TOKEN_KEY: 'token',
   OAUTH_CLIENT_ID: 1,
-  OAUTH_REDIRECT_URI: 'http://localhost:8090/login',
+  OAUTH_REDIRECT_URI: 'http://todosvue.pdavila.2dam.acacha.org/login',
   OAUTH_SERVER_URL: 'http://todosbackend.pdavila.2dam.acacha.org/oauth/authorize?',
   API_TASK_URL: 'http://todosbackend.pdavila.2dam.acacha.org/api/v1/task',
   API_PROFILE_URL: 'http://todosbackend.pdavila.2dam.acacha.org/api/v1/user',
