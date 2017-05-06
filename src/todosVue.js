869e51e90f8afb21030d36e3cb95352c6b8aa5ce
Paolo Dávila <pdavila@iesebre.com>
2017-05-06 13:42:47 +0200

    Change http to https

--- src/todosVue.js
+++ src/todosVue.js
@@ -2,14 +2,14 @@ const todosVue = {
   STORAGE_TOKEN_KEY: 'token',
   OAUTH_CLIENT_ID: 1,
   OAUTH_REDIRECT_URI: 'http://todosvue.pdavila.2dam.acacha.org/login',
-  OAUTH_SERVER_URL: 'http://todosbackend.pdavila.2dam.acacha.org/oauth/authorize?',
-  API_TASK_URL: 'http://todosbackend.pdavila.2dam.acacha.org/api/v1/task',
-  API_PROFILE_URL: 'http://todosbackend.pdavila.2dam.acacha.org/api/v1/user',
+  OAUTH_SERVER_URL: 'https://todosbackend.pdavila.2dam.acacha.org/oauth/authorize?',
+  API_TASK_URL: 'https://todosbackend.pdavila.2dam.acacha.org/api/v1/task',
+  API_PROFILE_URL: 'https://todosbackend.pdavila.2dam.acacha.org/api/v1/user',
   LATITUDE: 'latitude',
   LONGITUDE: 'longitude',
   ANDROID_SENDER_ID: '925822830182',
-  REGISTER_GCM_TOKEN_URL: 'http://todosbackend.pdavila.2dam.acacha.org/api/v1/user/gcmtoken',
-  GET_MESSAGES_URL: 'http://todosbackend.pdavila.2dam.acacha.org/api/v1/user/messages'
+  REGISTER_GCM_TOKEN_URL: 'https://todosbackend.pdavila.2dam.acacha.org/api/v1/user/gcmtoken',
+  GET_MESSAGES_URL: 'https://todosbackend.pdavila.2dam.acacha.org/api/v1/user/messages'
 }
 
 export default todosVue
